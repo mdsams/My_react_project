8e9bf36bd2591033cc13bf837c9a4d2573ab14f8
mdsams <tabrejmdsams786@gmail.com>
2021-08-30 00:58:54 +0530

    Task 1 initial

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props); 
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default DishDetail;
