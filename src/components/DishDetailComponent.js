ce91456fefb32eb78d7ac293dc1d118ceec4bb83
mdsams <tabrejmdsams786@gmail.com>
2021-09-27 23:54:02 +0530

    server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Button, Modal, ModalBody, ModalHeader, Label, Row, Col} from "reactstrap";
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
-
+import { baseUrl } from '../shared/baseUrl';
 
 
 /**........................ comment component Start ................................................. */
@@ -170,7 +170,7 @@ class CommentForm extends Component {
             return (
                 <div className='col-12 col-md-5 m-1'>
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
                             <CardTitle> {dish.name}</CardTitle>
                             <CardText> {dish.description} </CardText>
